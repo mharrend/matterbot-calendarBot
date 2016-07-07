8a74e45e9c2bb64797fea93e3abc343510bca69a
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2016-07-07 15:30:24 +0200

    Update calendarChecker.py
    Removed debug output

--- calendarBotProgram/calendarChecker.py
+++ calendarBotProgram/calendarChecker.py
@@ -26,7 +26,6 @@ def checkCalendarForUpcomingEvents():
     if successful:
         for item in res:
             eventContent = 'EventName: {0}\n EventBeschreibung: {1}\n EventStart: {2}\n EventEnd: {3}\n EventLocation: {4}\n EventCategory: {5}\n\n'.format(item.subject, html2text.html2text(item.body),item.start.astimezone(EWSTimeZone.timezone('Europe/Copenhagen')), item.end.astimezone(EWSTimeZone.timezone('Europe/Copenhagen')), item.location, item.categories)
-            print(item.categories)
             for subcalendar in item.categories:
                 try:
                     mattermostHook.send(eventContent, channel=subcalendar)
