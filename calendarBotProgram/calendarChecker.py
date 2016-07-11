f646cbb997c3b91603bc041590de5665b98f981c
felixriese <mail@felixriese.de>
2016-07-11 13:13:52 +0200

    changed the datetime string for output of calendarbot

--- calendarBotProgram/calendarChecker.py
+++ calendarBotProgram/calendarChecker.py
@@ -25,7 +25,7 @@ def checkCalendarForUpcomingEvents():
     successful, res = showAgenda('', nowDate.strftime("%d.%m.%Y %H:%M"), laterDate.strftime("%d.%m.%Y %H:%M"), True)
     if successful:
         for item in res:
-            eventContent = 'EventName: {0}\n EventBeschreibung: {1}\n EventStart: {2}\n EventEnd: {3}\n EventLocation: {4}\n EventCategory: {5}\n\n'.format(item.subject, html2text.html2text(item.body),item.start.astimezone(EWSTimeZone.timezone('Europe/Copenhagen')), item.end.astimezone(EWSTimeZone.timezone('Europe/Copenhagen')), item.location, item.categories)
+            eventContent = 'Upcoming event: {0}\n Details: {1}\n Time: {2} (KIT time)\n Location: {3}\n\n'.format(item.subject, html2text.html2text(item.body),item.start.strftime("%H:%M")), item.location)
             for subcalendar in item.categories:
                 try:
                     mattermostHook.send(eventContent, channel=subcalendar)
